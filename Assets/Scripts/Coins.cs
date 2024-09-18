e0d2b87aa195e217f2c4fa2cc2c3766341e69997
NghiaSionn <ptcn0120223@gmail.com>
2024-09-19 02:53:24 +0700

    commit4

---
+++ Assets/Scripts/Coins.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coins : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // Kiểm tra xem đối tượng chạm vào là "Player" (bóng) hay không
+        if (other.CompareTag("Player"))
+        {
+            // Gọi hàm spawn coin trong script SpawnCoin
+            SpawnCoin.instance.SpawnNewCoin();
+
+            // Hủy đối tượng coin sau khi chạm
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/SpawnCoin.cs
+++ Assets/Scripts/SpawnCoin.cs
@@ -1,32 +1,64 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnCoin : MonoBehaviour
 {
-    public GameObject theCoin;
+    public GameObject coinPrefab;
+    public Transform spawnArea;
+    public List<GameObject> walls;
 
-    // Start is called before the first frame update
+    public static SpawnCoin instance; // Singleton để dễ dàng gọi từ script khác
+
+    void Awake()
+    {
+        instance = this; // Gán đối tượng hiện tại cho instance
+    }
+
     void Start()
     {
-        
+        SpawnNewCoin(); // Spawn đồng xu đầu tiên
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SpawnNewCoin()
     {
-        gameObject.transform.Rotate(0f,1f,0f,Space.Self);    
-    }
+        Vector3 spawnPosition;
+        bool validPosition = false;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.tag == "Player" )
+        // Tạo vị trí spawn hợp lệ trên các đối tượng có tag là "Plane" và tránh các đối tượng có tag là "Wall"
+        while (!validPosition)
         {
-            Debug.Log("Triggered with: " + other.gameObject.tag);
-            Destroy(gameObject);
-            Vector3 randomPosition = new Vector3(Random.Range(-5, 5),1, Random.Range(-5, 5));
-            Debug.Log("Spawning coin at position: " + randomPosition);
-            Instantiate(theCoin, randomPosition, Quaternion.identity);
+            spawnPosition = new Vector3(
+                Random.Range(spawnArea.position.x - spawnArea.localScale.x / 2, spawnArea.position.x + spawnArea.localScale.x / 2),
+                spawnArea.position.y,
+                Random.Range(spawnArea.position.z - spawnArea.localScale.z / 2, spawnArea.position.z + spawnArea.localScale.z / 2)
+            );
+
+            Collider[] hits = Physics.OverlapSphere(spawnPosition, 0.5f);
+            bool isOnPlane = false;
+            bool collidesWithWall = false;
+
+            foreach (Collider hit in hits)
+            {
+                if (hit.CompareTag("Plane"))
+                {
+                    isOnPlane = true; // Vị trí hợp lệ nếu trên mặt phẳng "Plane"
+                }
+
+                if (hit.CompareTag("Wall"))
+                {
+                    collidesWithWall = true; // Vị trí không hợp lệ nếu chạm "Wall"
+                    break;
+                }
+            }
+
+            if (isOnPlane && !collidesWithWall)
+            {
+                validPosition = true;
+
+                // Tạo đồng xu tại vị trí hợp lệ
+                Instantiate(coinPrefab, spawnPosition, Quaternion.identity);
+            }
         }
     }
 }

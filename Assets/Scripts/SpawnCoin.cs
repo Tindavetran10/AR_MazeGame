98141aa1af1bb435804f090cd01dc2802cdc89d1
NghiaSionn <ptcn0120223@gmail.com>
2024-09-19 04:11:00 +0700

    commit 9
    material mine

--- Assets/Scripts/SpawnCoin.cs
+++ Assets/Scripts/SpawnCoin.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class SpawnCoin : MonoBehaviour
 {
     public GameObject coinPrefab;
-    public Transform[] spawnPoints; // Mảng các điểm spawn
+    public Transform[] spawnPoints; 
     public static SpawnCoin instance;
 
-    private int lastSpawnIndex = -1; // Biến lưu lại vị trí spawn trước đó để tránh trùng lặp
+    private int lastSpawnIndex = -1; 
 
     void Awake()
     {
@@ -22,23 +22,19 @@ public class SpawnCoin : MonoBehaviour
 
     public void SpawnNewCoin()
     {
-        int randomIndex = GetRandomSpawnIndex(); // Chọn điểm spawn ngẫu nhiên từ mảng
+        int randomIndex = GetRandomSpawnIndex();
 
-        // Lấy vị trí của điểm spawn ngẫu nhiên
         Vector3 spawnPosition = spawnPoints[randomIndex].position;
-
-        // Spawn đồng xu tại vị trí đã chọn
         Instantiate(coinPrefab, spawnPosition, Quaternion.identity);
     }
 
-    // Hàm chọn chỉ số spawn point ngẫu nhiên, tránh trùng với lần trước
     private int GetRandomSpawnIndex()
     {
         int randomIndex;
         do
         {
             randomIndex = Random.Range(0, spawnPoints.Length);
-        } while (randomIndex == lastSpawnIndex); // Đảm bảo không trùng với lần spawn trước
+        } while (randomIndex == lastSpawnIndex); 
 
         lastSpawnIndex = randomIndex;
         return randomIndex;
